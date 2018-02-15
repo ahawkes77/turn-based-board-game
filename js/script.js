1b36c0ed3de88dff2212aa1ed8fdcc5f55326295
Adrian <hawkes.adrian@gmail.com>
2018-02-15 14:39:31 +0000

    installation of npm canvas-shapes add in

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
- Volume in drive C is Acer
- Volume Serial Number is 42F8-C61E
-
- Directory of C:\Users\hawke\Desktop\Project 6 - Turn-based game
-
-12/02/2018  21:34    <DIR>          .
-12/02/2018  21:34    <DIR>          ..
-12/02/2018  21:34               361 index.html
-12/02/2018  21:34                 0 script.js
-12/02/2018  21:34               413 stylesheet.css
-               3 File(s)            774 bytes
-               2 Dir(s)  923,975,131,136 bytes free
+$(function(){
+    var options = {
+        shapeCount: 20,
+        timer: 100,
+        step: 3,
+        minSize: 50,
+        maxSize: 150
+    }
+var shapesInst = new Shapes('#ca', options)
+})
